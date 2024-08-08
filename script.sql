bf9405e12d99cd2f38fb10ac8ac607a01c1dd9f4
praveenpriyashan <praveenpriyashan@gmail.com>
2024-08-08 17:26:04 +0530

    add a user

--- script.sql
+++ script.sql
@@ -8,3 +8,9 @@ CREATE TABLE customer
     id   VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
+create table "user"(
+                       username varchar(100) primary key ,
+                       name varchar(200),
+                       password varchar(300)
+);
